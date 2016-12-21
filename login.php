cc875eeb87b3fe45ca0a9485a1ce7f533b0cdf4f
Aleksander Drozd <aleksander.drozd@outlook.com>
2016-12-21 14:21:17 +0100

    Working prototype of login feature

--- login.php
+++ login.php
@@ -1,6 +1,21 @@
 <?php
-    $login = $_POST['login'];
+    require_once "databaseConnect.php";
+
+    $email = $_POST['email'];
     $password = $_POST['password'];
 
-    echo $login."<br>";
-    echo $password."<br>";
+    $dbConnection = new mysqli($host, $dbUser, $dbPassword, $dbName);
+
+    if($dbConnection -> connect_errno != 0)
+        echo "Blad polaczenia z baza danych";
+    else{
+        $query = "Select * from customers where email='$email' and password='$password'";
+        $result = $dbConnection -> query($query);
+        $data = $result -> fetch_assoc();
+        echo $data['first_name']."<br>";
+        echo $data['last_name']."<br>";
+        $result -> free_result();
+        $dbConnection -> close();
+    }
+
+
