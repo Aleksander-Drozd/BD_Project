84a14d727e222b0df4252d9869ac8110067bce61
Aleksander Drozd <aleksander.drozd@outlook.com>
2016-12-24 21:02:10 +0100

    Added session variables and redirection to login handling file

--- login.php
+++ login.php
@@ -1,21 +1,31 @@
 <?php
     require_once "databaseConnect.php";
-
+    session_start();
     $email = $_POST['email'];
     $password = $_POST['password'];
 
-    $dbConnection = new mysqli($host, $dbUser, $dbPassword, $dbName);
+    $dbConnection = @new mysqli($host, $dbUser, $dbPassword, $dbName);
 
     if($dbConnection -> connect_errno != 0)
         echo "Blad polaczenia z baza danych";
     else{
         $query = "Select * from customers where email='$email' and password='$password'";
-        $result = $dbConnection -> query($query);
-        $data = $result -> fetch_assoc();
-        echo $data['first_name']."<br>";
-        echo $data['last_name']."<br>";
-        $result -> free_result();
+
+        if($result = @$dbConnection -> query($query)){
+            if($result -> num_rows == 1){
+                $data = $result -> fetch_assoc();
+
+                $_SESSION['firstName'] = $data['first_name'];
+                $_SESSION['lastName'] = $data['last_name'];
+                $_SESSION['wallet'] = $data['wallet'];
+                $_SESSION['rentedBikes'] = $data['rented_bikes'];
+
+                $result -> free_result();
+                header("Location: view/html/user.php");
+            }
+        }else
+            echo "Blad polaczenia z baza danych";
+
+
         $dbConnection -> close();
     }
-
-
